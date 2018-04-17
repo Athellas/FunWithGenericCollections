5b96df51f5db05d28a240c5922d3698348429afd
Athellas <mm.krupinski@gmail.com>
2018-04-17 15:00:47 +0200

    344

--- FunWithGenericCollections/Program.cs
+++ FunWithGenericCollections/Program.cs
@@ -11,6 +11,7 @@ namespace FunWithGenericCollections
         static void Main(string[] args)
         {
             UseGenericList();
+            UseGenericStack();
             Console.ReadLine();
         }
 
@@ -43,6 +44,32 @@ namespace FunWithGenericCollections
             for (int i = 0; i < arrayOfPeople.Length; i++)
             {
                 Console.WriteLine("First Names: {0}", arrayOfPeople[i].FirstName);
+            }
+        }
+
+        static void UseGenericStack()
+        {
+            Stack<Person> stackOfPeople = new Stack<Person>();
+            stackOfPeople.Push(new Person { FirstName = "Homer", LastName = "Simpson", Age = 47 });
+            stackOfPeople.Push(new Person { FirstName = "Marge", LastName = "Simpson", Age = 45 });
+            stackOfPeople.Push(new Person { FirstName = "Lisa", LastName = "Simpson", Age = 9 });
+
+            // Now look at the top item, pop it, and look again.
+            Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("\nFirst person item is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            try
+            {
+                Console.WriteLine("\nnFirst person is: {0}", stackOfPeople.Peek());
+                Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("\nError! {0}", ex.Message);
             }
         }
 
